31dd0d1eaf88f21ca851995541612812118f8f82
Cao Phuc <caophuc799@gmail.com>
2018-03-08 14:07:57 +0700

    part 2 run server in heroku

---
+++ Ex1/Phan 2/Server/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+var distance = require('google-distance');
+
+app.get('/', (req, res) => res.send('Cao Cao'))
+app.post('/location',(req,res)=>{
+    var origin =req.body.origin;// req.body.origin;
+    var destination =req.body.destination;// req.body.destination;
+    
+    distance.get({origin, destination}, function (err, distances) {
+        if (!err){
+            console.log(distances);
+            res.status(200).send(distances);
+         
+        }else{
+            console.log('Invalid!');
+            res.status(400).send("Invalid!");
+           
+        }
+    })
+});
+app.get('/glocation/a1/:origin/a2/:destination',(req,res)=>{
+  
+    var origin =req.param('origin');// req.body.origin;
+    var destination =req.param('destination');// req.body.destination;
+    
+    distance.get({origin, destination}, function (err, distances) {
+        if (!err){
+            console.log(distances);
+            res.status(200).send(distances);
+         
+        }else{
+            console.log('Invalid!');
+            res.status(400).send("Invalid!");
+           
+        }
+    })
+});
+
+
+app.listen(3000, () => console.log('Web service run in port 3000!'))
